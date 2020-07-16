817672f29b6ac3517cf875859afb27ae0fe0ca89
Fernando Figueiras <fjafig@gmail.com>
2020-07-16 23:31:18 +0100

    Api Products

---
+++ ShopAmazing.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopAmazing.Web.Data;
+
+namespace ShopAmazing.Web.Controllers.API//APi controller class empty na criacao
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller//Atencao mudar para controller so.O controller tb herda do controller base mas e mais especifico
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)//Injectamos a interface do product repository
+        {
+            _productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProducts()//mesmo que nao se ponha a notacao httpget ele funciona porque o nome e get, boa pratica por e vai ter mais metodos com o nome de Get.
+        {
+            return Ok(_productRepository.GetAll());//Como isto e um HTTP requeste passa as mensagem de HTTP da api
+            //Este OK envolve tudo o que ele recebe do GetAll e serializa tudo em Jason e retorna atravez da API.
+        }
+    }
+}
