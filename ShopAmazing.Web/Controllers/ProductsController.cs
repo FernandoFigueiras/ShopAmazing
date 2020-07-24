1a8890cedf87457710d81eaec0664dc0287d930a
Fernando Figueiras <fjafig@gmail.com>
2020-07-24 17:02:27 +0100

    Add and edit image

--- ShopAmazing.Web/Controllers/ProductsController.cs
+++ ShopAmazing.Web/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using ShopAmazing.Web.Data;
 using ShopAmazing.Web.Data.Entities;
 using ShopAmazing.Web.Helpers;
+using ShopAmazing.Web.Models;
 
 namespace ShopAmazing.Web.Controllers
 {
@@ -65,10 +67,39 @@ namespace ShopAmazing.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Product product)
+        public async Task<IActionResult> Create(/*Product product*/ ProductViewModel model)
         {
             if (ModelState.IsValid)
             {
+                //caminho para a pasta imagens dos produtos vindas do upload
+                var path = string.Empty;
+
+                if (model.ImageFile != null && model.ImageFile.Length>0)//se existe upload ou nao, pode ou nao por imagem
+                {
+
+                    var guid = Guid.NewGuid().ToString();
+                    var file = $"{guid}.jpg";
+
+
+                    //a pessoa carrrega a imagem mas o caminho vai para dentro do atributo da bd no image url do products, mas o ficheiro vai para dentro da pasta images / products
+                    path = Path.Combine(
+                        Directory.GetCurrentDirectory(),//directoria do servidor
+                        "wwwroot\\images\\Products",//o caminho
+                        /*model.ImageFile.FileName*/ file);//nome do ficheiro
+
+                    using (var stream = new FileStream(path, FileMode.Create))//cria o ficheiro
+                    {
+                        await model.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Products/{/*model.ImageFile.FileName*/ file}";//isto vai servir para guardar na base de dados o caminho da imagem ja guardada na pasta de imagens
+                    //Temos de alterar o mesmo nome do ficheiro usamos para isso o GUID
+                }
+
+                //agora temos de passar de um model para um product para guardar na base de dados, temos de converter
+
+                var product = this.ToProduct(model, path);//isto e um metodo nossoi que construimos
+
 
                 //por o user no product
                 //TODO: Change for logged user
@@ -78,13 +109,30 @@ namespace ShopAmazing.Web.Controllers
                 //_repository.AddProduct(product);
                 //await _repository.SaveAllAsync();
                 await _productRepository.CreateAsync(product);
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));//tambem queremos que o index tenha as imagens (View)
             }
-            return View(product);
+            return View(model);
+        }
+
+        private /*object*/ Product ToProduct(ProductViewModel model, string path)
+        {
+            //retorna um Product depois de ser feita a conversao para ser guardado na base de dadis
+            return new Product
+            {
+                Id = model.Id,
+                ImageUrl = path,
+                IsAvailable = model.IsAvailable,
+                LastPurchase = model.LastPurchase,
+                LastSale = model.LastSale,
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock,
+                User = model.User,
+            };
         }
 
         // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int? id)//queremos que a imagem ja esteja la quando se vai a view
         {
             if (id == null)
             {
@@ -97,7 +145,25 @@ namespace ShopAmazing.Web.Controllers
             {
                 return NotFound();
             }
-            return View(product);
+
+            var model = this.ToProductViewModel(product);//Queremos que a view ja tenha a imagem
+            return View(model);
+        }
+
+        private ProductViewModel ToProductViewModel(Product product)
+        {
+            return new ProductViewModel
+            {
+                Id = product.Id,
+                ImageUrl = product.ImageUrl,
+                IsAvailable = product.IsAvailable,
+                LastPurchase = product.LastPurchase,
+                LastSale = product.LastSale,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+                User = product.User,
+            };
         }
 
         // POST: Products/Edit/5
@@ -105,12 +171,37 @@ namespace ShopAmazing.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Product product)
+        public async Task<IActionResult> Edit(/*Product product*/ ProductViewModel model)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
+                    //aqui ja ha o caminho
+                    var path = model.ImageUrl;
+
+                    if (model.ImageFile != null && model.ImageFile.Length > 0)//se existe upload ou nao, pode ou nao por imagem
+                    {
+                        var guid = Guid.NewGuid().ToString();
+                        var file = $"{guid}.jpg";
+
+
+                        //a pessoa carrrega a imagem mas o caminho vai para dentro do atributo da bd no image url do products, mas o ficheiro vai para dentro da pasta images / products
+                        path = Path.Combine(
+                            Directory.GetCurrentDirectory(),//directoria do servidor
+                            "wwwroot\\images\\Products",//o caminho
+                            /*model.ImageFile.FileName*/ file);//nome do ficheiro
+
+                        using (var stream = new FileStream(path, FileMode.Create))//cria o ficheiro
+                        {
+                            await model.ImageFile.CopyToAsync(stream); //atencao que depois temos de apagar o ficheiro da pasta das imagens
+                        }
+
+                        path = $"~/images/Products/{/*model.ImageFile.FileName*/ file}";//isto vai servir para guardar na base de dados o caminho da imagem ja guardada na pasta de imagens
+                    }
+
+                    var product = this.ToProduct(model, path);
+
                     //TODO: Change for logged user
                     product.User = await _userHelper.GetUserByEmailAsync("fjfigdev@gmail.com");
                     //temos de por aqui porque caso corra mal no get como na view nao aparece campo nenhum para editar chamamos novamente o User
@@ -123,7 +214,7 @@ namespace ShopAmazing.Web.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!await _productRepository.ExistsAsync(product.Id))
+                    if (!await _productRepository.ExistsAsync(model.Id))
                     {
                         return NotFound();
                     }
@@ -134,7 +225,7 @@ namespace ShopAmazing.Web.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(model);
         }
 
         // GET: Products/Delete/5
---
+++ ShopAmazing.Web/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using ShopAmazing.Web.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopAmazing.Web.Models
+{
+    public class ProductViewModel : Product //Porque vamos utilizar esta view para chamar as imagens a gravar mas que nao vao ser gravadas na base de dados e uma view especial para este modelo de dados
+    {   //Um objecto proprio so para a view - E o que esta entre o modelo e a view
+        //vai buscar os ficheiros
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; } //vamos alterar a view do create dos produtos
+
+    }
+}
